787c2be4c4a0fe463dbead272329d62256eb9242
Harang <dbd02169@naver.com>
2025-06-22 15:14:53 +0900

    fix(web): broken web build config (#33)

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    },
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  }
 })
