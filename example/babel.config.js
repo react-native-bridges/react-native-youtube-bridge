3ae09aeceac492c3e379c735c918d1d1a1fdeaa4
saseungmin <dbd02169@naver.com>
2025-06-24 13:05:39 +0900

    fix: migrate package manager bun to pnpm

--- example/babel.config.js
+++ example/babel.config.js
@@ -3,19 +3,5 @@ module.exports = (api) => {
 
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            'react-native-youtube-bridge': '../packages/react-native-youtube-bridge/src/index',
-            '@react-native-youtube-bridge/core': '../packages/core/src/index',
-            '@react-native-youtube-bridge/react': '../packages/react/src/index',
-          },
-          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-        },
-      ],
-    ],
   };
 };
